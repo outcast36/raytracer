b82efd893645e06f8d434071ffa010f306d71469
Wyatt Ayers <ayersw2@wwu.edu>
2022-12-12 13:38:55 -0800

    Hemisphere sampling method

--- RayTracer/src/GfxBase.jl
+++ RayTracer/src/GfxBase.jl
@@ -1,6 +1,6 @@
 module GfxBase
 
-export Vec3, Vec2, Ray
+export Vec3, Vec2, Ray, randomUnitSphere, randomHemiSphere
 
 using Images
 using StaticArrays
@@ -22,6 +22,26 @@ end
 """ Return a random value in the range [min, max) """
 function random_float(low, high)
     return low + (high-low)*rand();
+end
+
+""" Rejection method for picking a random point in the unit sphere """
+function randomUnitSphere()
+    while(true)
+        p = Vec3(random_float(-1,1), random_float(-1,1), random_float(-1,1))
+        if (norm(p) < 1)
+            return p
+        end
+    end
+end
+
+""" Uniformly generate points on the hemisphere around a normal """
+function randomHemiSphere(normal::Vec3)
+    inSphere = randomUnitSphere()
+    if (dot(inSphere, normal) > 0.0) #In the same hemisphere as the normal
+        return inSphere;
+    else
+        return -inSphere;
+    end
 end
 
 end # module GfxBase
