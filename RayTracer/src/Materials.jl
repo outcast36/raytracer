50cfc97032e4d92c169d604ace948afb47627aee
Wyatt Ayers <ayersw2@wwu.edu>
2022-12-23 22:05:14 -0800

    Added Glossy material

--- RayTracer/src/Materials.jl
+++ RayTracer/src/Materials.jl
@@ -7,26 +7,31 @@ using ..GfxBase
 using ..Lights
 
 export Material
-export Lambertian, Metallic, Dielectric
+export Lambertian, Metallic, Dielectric, Glossy
 
 abstract type Material end
 
 mutable struct Lambertian <: Material
     albedo::RGB{Float32}
-    specularColor::RGB{Float32}
+    specularColor::RGB{Float32} 
     specularExp
 end
 
 mutable struct Metallic <: Material
     albedo::RGB{Float32}
     specularColor::RGB{Float32}
-    reflectance::Float64
-    fuzz::Float64
     specularExp
 end
 
 mutable struct Dielectric <: Material
     index::Float64
+end
+
+mutable struct Glossy <: Material
+    albedo::RGB{Float32}
+    specularColor::RGB{Float32} 
+    reflectance::Float64 
+    specularExp
 end
 
 end
