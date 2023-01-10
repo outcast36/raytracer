9bf330da691a4c1cdd81a28fbeb56823a2564751
Wyatt Ayers <ayersw2@wwu.edu>
2023-01-09 16:16:30 -0800

    Area light sampling from pbr

--- RayTracer/src/Lights.jl
+++ RayTracer/src/Lights.jl
@@ -30,23 +30,37 @@ end
 
 """ Sample a spherical light-source on the hemisphere visible from the point """
 function sampleAreaLight(light::AreaLight, point::Vec3)
-    #Create a basis where w is the vector from the light's center to the point
-    w = normalize(light.center - point)
-    #Create non-colinear vector t
-    t = [w[1], w[2], w[3]]
-    t[argmin(w)] = 1
-    t = Vec3(t[1], t[2], t[3])
-
-    u = normalize(cross(t,w))
-    v = cross(w, u)
-
-    if (norm(light.center - point) < light.radius^2)
-        return sampleSphere() #point inside light source
+    radius = light.radius
+    sp = Vec2(rand(), rand()) #Two canonical random numbers
+    if (norm(light.center - point)^2 < radius^2)
+        return uniformSampleSphere(sp) #point inside light source
     else
+        #Create a basis where w is the vector from the light's center to the point
+        w = normalize(light.center - point)
+        #Create non-colinear vector t
+        t = [w[1], w[2], w[3]]
+        t[argmin(w)] = 1
+        t = Vec3(t[1], t[2], t[3])
+        
+        u = normalize(cross(t,w))
+        v = cross(w, u)
+
         #compute theta and phi values for sample in cone
+        sinThetaMax2 = (radius^2)/(dot(light.center - point, light.center - point))
+        cosThetaMax = sqrt(max(0, 1 - sinThetaMax2))
+        cosTheta = (1 - sample[1]) + sp[1] * cosThetaMax
+        sinTheta = sqrt(max(0, 1 - cosTheta^2))
+        phi = 2*pi*sp[2]
+
+        #distTocenter = norm(light.center - point)
+        #distToSurface = distTocenter * cosTheta - sqrt(radius^2 - (distTocenter^2 * sinTheta^2))
+        #cosAlpha = (distTocenter^2+radius^2-distToSurface^2)/(2*radius*distTocenter)
+        #sinAlpha = sqrt(max(0, 1 - cosAlpha^2))
         #compute angle alpha from center of sphere to sampled point on surface
+        normal = -sinTheta*cos(phi)*u - sinTheta*sin(phi)*v - cosTheta*w
+        sampledLight = radius * normal
         #compute surface normal and sampled point on sphere
-        #return hitRecord for sampled point on sphere
+        return sampledLight
     end
 end
 
@@ -58,7 +72,7 @@ function light_direction(light::DirectionalLight, point::Vec3)
 end
 
 function light_direction(light::AreaLight, point::Vec3)
-    samplePoint = sampleAreaLight(point)#randomly sample point on light geometry
+    samplePoint = sampleAreaLight(light, point)#randomly sample point on light geometry
     return normalize(samplePoint - point) #direction from point to light source; normalized
 end
 
