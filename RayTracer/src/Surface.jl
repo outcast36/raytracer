5570c4964e77d63397776023358878c8f884a28c
Wyatt Ayers <ayersw2@wwu.edu>
2023-02-21 18:16:27 -0800

    Copied triangle code from scenes

--- RayTracer/src/Surface.jl
+++ RayTracer/src/Surface.jl
@@ -3,6 +3,10 @@ module Surface
 export Sphere, HitRecord, ray_intersect
 using LinearAlgebra
 
+using ..GfxBase
+using ..MeshGen
+using ..Materials
+
 """ Structure to store data about an intersection
 of a ray with an object (a "hit")."""
 mutable struct HitRecord
@@ -12,15 +16,45 @@ mutable struct HitRecord
     object
 end
 
-""" Sphere object """
 struct Sphere
     center::Vec3
     radius::Float64
     material::Material
 end
 
-# Abstract ray-object intersection function:
-# Needs to be implemented for each type of object to be rendered
+struct Triangle
+    geometry::OBJTriangle
+    mesh::OBJMesh
+    material::Material
+end
+
+""" Return an Array of all Triangles belonging to the given mesh, assigning
+each one the given material. """
+function create_triangles(mesh::OBJMesh, material)
+    [Triangle(f, mesh, material) for f in mesh.triangles]
+end
+
+""" Some helper functions that make for easier access to triangle data: """
+function get_vertex(tri::Triangle, i)
+    tri.mesh.positions[tri.geometry.positions[i]]
+end
+
+function has_uvs(tri::Triangle)
+    length(tri.geometry.uvs) == 3
+end
+
+function get_uv(tri::Triangle, i)
+    tri.mesh.uvs[tri.geometry.uvs[i]]
+end
+
+function has_normals(tri::Triangle)
+    length(tri.geometry.normals) == 3
+end
+
+function get_normal(tri::Triangle, i)
+    tri.mesh.normals[tri.geometry.normals[i]]
+end
+
 """ Intersect a ray with an object.
 Returns a HitRecord with info about the intersetion, or nothing if
 the ray doesn't intersect with the object. """
@@ -49,4 +83,9 @@ function ray_intersect(object::Sphere, ray::Ray)
     end
 end
 
+function ray_intersect(object::Triangle, ray::Ray)
+    
+    return HitRecord(t, intersection, normal, uv, object)
 end
+
+end #Module Surface
