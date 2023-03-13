f947ab9f3eba65cf87232627591f8e873716df18
Wyatt Ayers <ayersw2@wwu.edu>
2023-03-12 20:53:12 -0700

    No more  nothing materials

--- RayTracer/src/Scenes.jl
+++ RayTracer/src/Scenes.jl
@@ -28,7 +28,7 @@ struct Sphere
     center::Vec3
     radius::Float64
     emission::RGB{Float32} #If surface is a light emitting surface (a luminaire) this will be nonzero
-    material::Union{Material, Nothing}
+    material
 end
 
 """ Data type: stores the OBJTriangle, a reference to its Mesh
