243ced51e1e31ed030333a638cca62a8348597ef
Wyatt Ayers <ayersw2@wwu.edu>
2023-03-29 11:00:44 -0700

    New test scene 1 + cornell box start

--- RayTracer/src/TestScenes.jl
+++ RayTracer/src/TestScenes.jl
@@ -17,6 +17,8 @@ PURPLE = RGB{Float32}(0.55,0.47,1)
 LEMON = RGB{Float32}(1.0, 1.0, 0.3)
 PINK = RGB{Float32}(0.7, 0.3, 0.3)
 LIME = RGB{Float32}(0.33, 0.97, 0.26)
+SILVER = RGB{Float32}(0.8, 0.8, 0.8)
+GOLD = RGB{Float32}(0.8, 0.6, 0.2)
 
 """ Take the OBJMesh mesh and return an array of Triangles from the mesh
 with the given material, after scaling the mesh positions by scale and moving
@@ -38,7 +40,7 @@ function camera_2(img_height, img_width)
     eye = Vec3(20, 4, 10)
     view = Vec3(-1, 0, -5) - eye
     up = Vec3(0, 1, 0)
-    focal = 13.0
+    focal = 8.0
     apt = 16.0
     Cameras.PerspectiveCamera(eye, view, up, focal, apt, img_height, img_width)
 end
@@ -77,6 +79,19 @@ function camera_5(img_height, img_width)
         img_width)
 end
 
+#Parameters sourced from: https://www.graphics.cornell.edu/online/box/data.html
+function cornellBoxCam(img_height, img_width)
+    Cameras.PerspectiveCamera(
+        Vec3(278, 273, -800), #eye
+        Vec3(0,0,0), #u_axis
+        Vec3(0,1,0), #up
+        Vec3(0,0,1), #view
+        0.035, #focal length
+        4.0, #aperture
+        img_height,
+        img_width
+    )
+end
 
 cameras = [camera_1, camera_2, camera_3, camera_4, camera_5]
 
@@ -89,26 +104,40 @@ function get_scene(i)
     scenes[i]()
 end
 
-""" Metallic spheres on the left and right, with a diffuse red sphere in the center """ 
+function cornellBox()
+bg = BLACK 
+mat_light = Lambertian(WHITE, WHITE, 10)
+objs = []
+#floor
+whiteMat = Lambertian(WHITE, WHITE, 10)
+floor = read_obj("meshes/cb/floor.obj")
+append!(objs, mesh_helper(floor, whiteMat))
+push!(objs, Sphere(Vec3(0,2.0,0.0), 1.0, WHITE, mat_light)) #Light source
+
+lights = [AreaLight(objs[2])] 
+Scene(bg, objs, lights)
+end
+
+""" Red, blue, and green lambertian spheres illuminated by a sphereical light behind the camera """ 
 function scene_1()
-    bg = RGB{Float32}(0.5, 0.7, 1.0)
-    ground_color = RGB{Float32}(0.8, 0.8, 0.0)
-    left_metal = RGB{Float32}(0.8, 0.8, 0.8)
-    right_metal = RGB{Float32}(0.8, 0.6, 0.2)
-    lightColor = RGB{Float32}(0.7, 0.7, 0.7)
+    bg = BLACK
+    #ground_color = RGB{Float32}(0.8, 0.8, 0.0)
 
     objs = []
+    mat_left = Lambertian(RGB{Float32}(0.5, 0.7, 1.0), BLUE, 10) 
+    mat_right = Lambertian(LIME, GREEN, 10)
     mat_center = Lambertian(RGB{Float32}(0.7, 0.3, 0.3), RED, 10)
-    mat_left = Metallic(left_metal, left_metal, 100)
-    mat_right = Metallic(right_metal, right_metal, 100)
-    mat_ground = Lambertian(ground_color, WHITE, 10)
+    mat_ground = Lambertian(WHITE, WHITE, 10)
+    mat_light = Lambertian(WHITE, WHITE, 10)
 
-    push!(objs, Sphere(Vec3(0, 0.0, -3.0), 0.5, mat_center))
-    push!(objs, Sphere(Vec3(-1.0, 0.0, -3.5), 0.5, mat_left))
-    push!(objs, Sphere(Vec3(1.0, 0.0, -3.5), 0.5, mat_right))
-    push!(objs, Sphere(Vec3(0,-101,-1), 100.0, mat_ground))
-    lights = [AreaLight(lightColor, Vec3(1.5, 1.5, 0.0), 2.0)]
-    #lights = [DirectionalLight(lightColor, Vec3(0, 1, 1)), PointLight(lightColor, Vec3(1,0,0))]
+    push!(objs, Sphere(Vec3(-1.0, 0.0, -3.5), 0.5, BLACK, mat_left)) #Blue
+    push!(objs, Sphere(Vec3(0, 0.0, -3.0), 0.5, BLACK, mat_center)) #Red
+    push!(objs, Sphere(Vec3(1.0, 0.0, -3.5), 0.5, BLACK, mat_right)) #Green
+    push!(objs, Sphere(Vec3(0,-101,-1), 100.0, BLACK, mat_ground))
+    push!(objs, Sphere(Vec3(0,3.0,3), 1.0, WHITE, mat_light)) #Light source
+    push!(objs, Sphere(Vec3(0,3.0,-8), 1.0, WHITE, mat_light)) #Light source
+
+    lights = [AreaLight(objs[5]), AreaLight(objs[6])] 
     Scene(bg, objs, lights)
 end
 
@@ -125,12 +154,13 @@ function scene_2()
     mat_right = Metallic(right_metal, right_metal, 100)
     mat_ground = Lambertian(ground_color, WHITE, 10)
 
-    push!(objs, Sphere(Vec3(0, 0.0, -3.0), 0.5, mat))
-    push!(objs, Sphere(Vec3(-1.0, 0.0, -3.5), 0.5, mat_left))
-    push!(objs, Sphere(Vec3(1.0, 0.0, -3.5), 0.5, mat_right))
-    push!(objs, Sphere(Vec3(0,-101,-1), 100.0, mat_ground))
+    push!(objs, Sphere(Vec3(0, 0.0, -3.0), 0.5, mat, BLACK))
+    push!(objs, Sphere(Vec3(-1.0, 0.0, -3.5), 0.5, mat_left, BLACK))
+    push!(objs, Sphere(Vec3(1.0, 0.0, -3.5), 0.5, mat_right, BLACK))
+    push!(objs, Sphere(Vec3(0,-101,-1), 100.0, mat_ground, BLACK))
+    push!(objs, Sphere(Vec3(1.5, 1.5, 0.0), 2.0, nothing, BLACK))
 
-    lights = [AreaLight(lightColor, Vec3(1.5, 1.5, 0.0), 2.0)]
+    lights = [AreaLight(lightColor, )]
     #lights = [DirectionalLight(lightColor, Vec3(0, 1, 1)), PointLight(lightColor, Vec3(1,0,0))]
     Scene(bg, objs, lights)
 end
@@ -161,23 +191,22 @@ end
 """ RGB spheres on the ground with big reflective sphere on the right """
 function scene_4()
     bg = BLACK
-    lightColor = RGB{Float32}(0.5, 0.5, 0.5)
+    lb = RGB{Float32}(0.5, 0.7, 1.0)
 
     r = Lambertian(RED, WHITE, 10)
     g = Lambertian(GREEN, WHITE, 10)
     b = Lambertian(BLUE, WHITE, 10)
-    refl = Metallic(WHITE, WHITE, 10)
+    refl = Lambertian(WHITE, WHITE, 10)#Metallic(WHITE, WHITE, 10)
 
-    objs = [Sphere(Vec3(-1,  -1.1, -3), 0.5, r),
-            Sphere(Vec3( -0.5,  -1.0, -4), 0.5, g),
-            Sphere(Vec3( 0,  -1.0, -5), 0.5, b),
-            Sphere(Vec3( 5,  -1, -4), 4, refl),
-            Sphere(Vec3(  0, -101, 0), 100, refl) # floor
+    objs = [Sphere(Vec3(-1,  -1.1, -3), 0.5, BLACK, r),
+            Sphere(Vec3( -0.5,  -1.0, -4), 0.5, BLACK, g),
+            Sphere(Vec3( 0,  -1.0, -5), 0.5, BLACK, b),
+            Sphere(Vec3( 5,  -1, -4), 4, BLACK, refl),
+            Sphere(Vec3(  0, -101, 0), 100, BLACK, refl), 
+            Sphere(Vec3(0,0,0), 0.8, WHITE, refl), #light
            ]
 
-    lights = [AreaLight(lightColor, Vec3(1.5, 1.5, 0.0), 2.0)]
-    #lights = [DirectionalLight(lightColor, Vec3(-1, 2, 1)), PointLight(lightColor, Vec3(-4, -1, 2))]
-
+    lights = [AreaLight(objs[6])]
     Scene(bg, objs, lights)
 end
 
@@ -236,7 +265,14 @@ function scene_7()
     cube_mat = Metallic(WHITE, WHITE, 10)
     append!(objs, mesh_helper(cube_mesh(), cube_mat, 10.0, Vec3(-11.2, 0, 0)))
 
-    lights = [AreaLight(lightColor, Vec3(1.5, 1.5, 0.0), 2.0)] #
+    #lights = [AreaLight(lightColor, Vec3(1.5, 1.5, 0.0), 2.0)] #
+
+    lights = [ PointLight(0.5, Vec3(1,2,-5)),
+               DirectionalLight(0.3, Vec3(0,0,1)),
+               DirectionalLight(0.3, Vec3(0,1,1)),
+               DirectionalLight(0.3, Vec3(1,1,1)),
+               DirectionalLight(0.3, Vec3(0,1,0)) ]
+
 
     Scene(bg, objs, lights)
 
